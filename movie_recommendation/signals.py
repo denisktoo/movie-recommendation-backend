2faec589dbce2110fc35820418a40b81b9020e0d
denisktoo <146120658+denisktoo@users.noreply.github.com>
2026-04-13 11:57:55 +0300

    refactor: remove obsolete recommendation cache signal

--- movie_recommendation/signals.py
+++ movie_recommendation/signals.py
@@ -1,8 +1,2 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, RecommendationCache
-
-@receiver(post_save, sender=User)
-def create_recommendation_cache(sender, instance, created, **kwargs):
-    if created:
-        RecommendationCache.objects.create(user=instance, data={})
