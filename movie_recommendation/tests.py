016aa9278d13838e287121b7771ec4ebd13f67fe
denisktoo <146120658+denisktoo@users.noreply.github.com>
2026-04-17 17:00:10 +0300

    fix: add placeholder test

--- movie_recommendation/tests.py
+++ movie_recommendation/tests.py
@@ -1,3 +1,6 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# # Create your tests here.
+
+class BasicTestCase(TestCase):
+    def test_placeholder(self):
+        self.assertTrue(True)
